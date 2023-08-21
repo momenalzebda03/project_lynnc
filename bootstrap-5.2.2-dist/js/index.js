ec17c7862334fc6d60d57b44c6017c73bd0cd6f1
unknown <momen07996@gmail.com>
2023-08-21 17:51:39 +0300

    create section main

--- bootstrap-5.2.2-dist/js/index.js
+++ bootstrap-5.2.2-dist/js/index.js
@@ -33,14 +33,31 @@ $(document).ready(function () {
       link_color.classList.add("active");
     }
   );
-  $(".div_section_block").hover(
+  $(".li_overflow").hover(
     function () {
-      $(this).parent().find(".link_color").addClass("active");
       link_color.classList.remove("active");
     },
     function () {
-      $(this).parent().find(".link_color").removeClass("active");
       link_color.classList.add("active");
     }
   );
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const divSectionBlocks = document.querySelectorAll(".div_section_block");
+  const sectionSolutions = document.querySelectorAll(".section_solutions");
+  divSectionBlocks.forEach((block, index) => {
+    block.addEventListener("mouseover", function () {
+      sectionSolutions[index].classList.add("flex-display");
+    });
+    block.addEventListener("mouseout", function () {
+      sectionSolutions[index].classList.remove("flex-display");
+    });
+    sectionSolutions[index].addEventListener("mouseover", function () {
+      this.classList.add("flex-display");
+    });
+    sectionSolutions[index].addEventListener("mouseout", function () {
+      this.classList.remove("flex-display");
+    });
+  });
 });
