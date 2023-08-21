4ee4d0884c922f4a969b484429fddaa31a77ffaa
unknown <momen07996@gmail.com>
2023-08-21 03:44:36 +0300

    end navbar

--- bootstrap-5.2.2-dist/js/index.js
+++ bootstrap-5.2.2-dist/js/index.js
@@ -1,6 +1,7 @@
+var button_click = document.getElementById("button_click");
 var menu = document.getElementById("menu");
 var bollen = true;
-menu.addEventListener("click", () => {
+button_click.addEventListener("click", () => {
   if (bollen == true) {
     menu.classList = "fas fa-times";
     bollen = false;
@@ -8,4 +9,38 @@ menu.addEventListener("click", () => {
     menu.classList = "fas fa-bars";
     bollen = true;
   }
+});
+
+var link_color = document.getElementById("link_color");
+$(document).ready(function () {
+  $(".section_solutions").hover(
+    function () {
+      $(this).parent().find(".link_color").addClass("active");
+      link_color.classList.remove("active");
+    },
+    function () {
+      $(this).parent().find(".link_color").removeClass("active");
+      link_color.classList.add("active");
+    }
+  );
+  $(".link_color").hover(
+    function () {
+      $(this).parent().find(".link_color").addClass("active");
+      link_color.classList.remove("active");
+    },
+    function () {
+      $(this).parent().find(".link_color").removeClass("active");
+      link_color.classList.add("active");
+    }
+  );
+  $(".div_section_block").hover(
+    function () {
+      $(this).parent().find(".link_color").addClass("active");
+      link_color.classList.remove("active");
+    },
+    function () {
+      $(this).parent().find(".link_color").removeClass("active");
+      link_color.classList.add("active");
+    }
+  );
 });
